9962ba73dbdf9ebea8166285c1923dbe7801eb6d
KalitaSerj <167794708+KalitaSerj@users.noreply.github.com>
2024-06-01 17:48:03 +0300

    Add files via upload

---
+++ module 5_1.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(f"Едем на этаж {floor}")
+
+
+elbrus = House('ЖК Эльбрус', 30)
+elbrus.go_to(29)
